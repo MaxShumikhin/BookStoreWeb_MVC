26f012e08210599aa37c5f8f2119087e801a242e
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2024-02-26 22:08:15 +0100

    Added controller for Category and view. Added  operation for show all categories.

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStoreWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _appDbContext;
+        public CategoryController(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categoryList = _appDbContext.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
