fd25907670c7472765489a53cd3d74f0d1fbb728
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2024-02-26 21:15:33 +0100

    Added code for create and connection to database.

---
+++ BookStoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
