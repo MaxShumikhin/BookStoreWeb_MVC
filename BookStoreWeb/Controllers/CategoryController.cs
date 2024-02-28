6eb2773f4f75b0df72c449de434d811a8918bdab
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2024-02-28 21:38:32 +0100

    Added edit logic for code and view for update

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -36,5 +36,47 @@ namespace BookStoreWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _appDbContext.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order can't exactly match the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Categories.Update(category);
+                _appDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+
+
+
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
     }
 }
