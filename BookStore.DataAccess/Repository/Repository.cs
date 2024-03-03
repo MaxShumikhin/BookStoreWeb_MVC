99fe2164abeceb5ad4dfb68629653837943f791b
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2024-03-03 21:29:52 +0100

    Create IRepository and implemented this interface

---
+++ BookStore.DataAccess/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStoreWeb.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
