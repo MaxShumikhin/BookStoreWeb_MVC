09f859c89c5d4b93b362ac9e98f0fd1ba6e0f962
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2024-03-03 21:39:46 +0100

    Added ICategoryRepository and implemented

---
+++ BookStore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using BookStoreWeb.DataAccess.Data;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) :base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+    }
+}
